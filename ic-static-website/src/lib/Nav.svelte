5557de241a8fc0178243c54c76df767a73c15c01
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-10-03 18:28:53 +0900

    ヘッダーとフッターを追加しました。

---
+++ ic-static-website/src/lib/Nav.svelte
@@ -0,0 +1,12 @@
+<header class="bg-blue-400 p-4 sticky top-0">
+    <nav class="container flex justify-between items-center mx-auto">
+        <!-- Title -->
+        <div class="text-2xl">MY Portfolio App</div>
+        <div class="space-x-12">
+            <!-- Link -->
+            <a href="#home" class="hover:text-gray-500">HOME</a>
+            <a href="#about" class="hover:text-gray-500">ABOUT</a>
+            <a href="#portfolio" class="hover:text-gray-500">PORTFOLIO</a>
+        </div>
+    </nav>
+</header>
